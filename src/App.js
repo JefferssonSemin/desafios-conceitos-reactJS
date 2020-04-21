d4c864aa533525f3a671f951aeea1478fa85886e
JefferssonSemin <jeffesemin@hotmail.com>
2020-04-21 20:46:08 -0300

    Desenvolvido o teste em cima do template

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  // Utilizado o "componentDidUpdate" para realizar a atualização simultânea assim que o repositories sofrer alteração.
+  useEffect(() =>{
+     api.get('repositories').then(repository => {
+      setRepositories(repository.data);
+    })
+  }, [repositories])
+
   async function handleAddRepository() {
-    // TODO
+  // Poderia montar um objeto e por no lugar do JSON, daria pra fazer de diversas maneiras.
+  // Poderia tratar o retorno do request, porém deixei conforme pediu o teste.
+
+   await api.post('repositories', {
+    "title": "Desafio teste",
+    "url": "https://github.com/JefferssonSemin/desafio-conceitos-node.git",
+    "techs": ["nodejs", "reactjs"]
+   });
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+  // Poderia tratar o retorno do request, porém deixei conforme pediu o teste.
+    await api.delete(`repositories/${id}`);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repo => 
+        <li key={repo.id}>
+        {repo.title}
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
         </li>
+        )}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
