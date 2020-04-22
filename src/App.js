ad620184acbc88fcda04c275c2c74ea75587a65b
JefferssonSemin <jeffesemin@hotmail.com>
2020-04-22 13:43:07 -0300

    Retirado atualizacao por useEffect

--- src/App.js
+++ src/App.js
@@ -7,27 +7,28 @@ import "./styles.css";
 function App() {
   const [repositories, setRepositories] = useState([]);
 
-  // Utilizado o "componentDidUpdate" para realizar a atualização simultânea assim que o repositories sofrer alteração.
   useEffect(() =>{
      api.get('repositories').then(repository => {
       setRepositories(repository.data);
     })
-  }, [repositories])
+  }, [])
 
   async function handleAddRepository() {
-  // Poderia montar um objeto e por no lugar do JSON, daria pra fazer de diversas maneiras.
-  // Poderia tratar o retorno do request, porém deixei conforme pediu o teste.
-
-   await api.post('repositories', {
+ 
+   const response = await api.post('repositories', {
     "title": "Desafio teste",
     "url": "https://github.com/JefferssonSemin/desafio-conceitos-node.git",
     "techs": ["nodejs", "reactjs"]
    });
+
+   setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-  // Poderia tratar o retorno do request, porém deixei conforme pediu o teste.
-    await api.delete(`repositories/${id}`);
+   await api.delete(`repositories/${id}`);
+  
+   const repository = repositories.filter(repository => repository.id !== id)
+   setRepositories(repository)
   }
 
   return (
